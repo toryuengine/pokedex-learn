f3202d570543f91bf624653b8b564f2c44399ad1
toryuengine <toryuengine@gmail.com>
2026-04-14 00:08:53 +0900

    main-commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+class pokemon_character {
+    constructor(name, type, level,hp, attack, defense) {
+        this.name = name;
+        this.type = type;
+        this.hp = hp;
+        this.attack = attack;
+        this.defense = defense;
+    }  
+}
+
+
+correct_status() {
+
+}
